a0444f19c78da89bc5ac45afad9f6c2a431a12c0
Catalina S.I <c.santamaria1@uniandes.edu.co>
2025-11-24 15:29:29 -0500

    avance

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { ArstistaList } from './arstista-list/arstista-list';
+import { AlbumListComponent } from './album/album-list/album-list.component';
 
 @NgModule({
   declarations: [AppComponent, ArstistaList],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule
+    ,AlbumListComponent
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
